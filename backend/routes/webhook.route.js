b41ab2fdda1b3fd14682acbbe1f4ac529a9fa512
Kenil-Mangukiya <kenilmangukiya033@gmail.com>
2025-10-14 16:35:28 +0530

    Remove all frontend and backend files, including configurations, components, and dependencies, effectively resetting the project structure.

--- backend/routes/webhook.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import webhook from "../controllers/webhook.controller.js";
-
-const router = express.Router();
-
-router.post("/webhook", webhook);
-
-export default router;
