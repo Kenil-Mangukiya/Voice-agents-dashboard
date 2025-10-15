ebbbdf61a271d9533d40be5124a238925ab80a5f
Ubuntu <ubuntu@ip-172-31-22-241.ec2.internal>
2025-10-15 04:21:29 +0000

    chnages in the server

---
+++ backend/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import config from "../config/config.js"
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${config.mongodbUri}`);
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("fail to connect with MongoDB", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
+
