fd019d59caaf31ce67c54c22a9b2044d23fe1e5b
Ubuntu <ubuntu@ip-172-31-22-241.ec2.internal>
2025-10-16 12:53:54 +0000

    made server changes

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,18 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import config from "./config/config.js";
 import webhookRoute from "./routes/webhook.route.js";
 import callsRoute from "./routes/calls.route.js";
 import userRoute from "./routes/user.route.js";
 
 const app = express();
 
-app.use(cors({
-    origin: config.corsOrigin,
-    credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["https://dashboard.aiyug.us","https://albuquerquedemo.harm.ai"], 
+    credentials: true
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
--- backend/config/config.js
+++ backend/config/config.js
@@ -8,7 +8,7 @@ const config = {
     mongodbUri: process.env.MONGODB_URI,
     dbName: process.env.DB_NAME,
     port: process.env.PORT,
-    corsOrigin: process.env.CORS_ORIGIN.split(","),
+    corsOrigin: process.env.CORS_ORIGIN.split(",").map(o => o.trim()),
     apiBaseUrl: process.env.API_BASE_URL,
     accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
     accessTokenExpire: process.env.ACCESS_TOKEN_EXPIRE,
